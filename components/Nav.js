8956df1db4793d7d85590d7b24341f42b9d8c811
Marty de Viterbo <mmdeviterbo@up.edu.ph>
2021-06-23 00:35:17 +0800

    FEAT: finish navbar

--- components/Nav.js
+++ components/Nav.js
@@ -1,33 +1,22 @@
 import React from 'react'
 import Link from 'next/link';
-import dynamic from 'next/dynamic'
-
-import { loadAnimation } from "lottie-web";
-import { defineLordIconElement } from "lord-icon-element";
-
-// register lottie and define custom element
-defineLordIconElement(loadAnimation);
-
-const DynamicComponent = dynamic(() => import('lottie-web'))
-
+import Image from 'next/image';
+import logo from '../public/assets/logo.png'
 
 export default function Nav() {
     return (
         <div id="nav-container">
+            <div id="nav-container-start">
+                <Image src={logo} alt="Logo" height={40} width={40} id="logo"/>
+                <p>DE VITERBO</p>
+            </div>
+
             <div id="nav-container-end">
                 <Link href="/">
-                    <a className="concat">Contact</a>
+                    <a className="contact">Contact</a>
                 </Link>
                 <div className="menu-icon-container">
-                    <DynamicComponent/>
-                    <script src="https://cdn.lordicon.com/libs/frhvbuzj/lord-icon-2.0.2.js"/>
-                    <lord-icon
-                        src="https://cdn.lordicon.com/wgwcqouc.json"
-                        trigger="click"
-                        colors="primary:#ffffff,secondary:#ffffff"                        
-                        stroke="78"
-                        delay="100">
-                    </lord-icon>
+                    <i className="fa fa-lg fa-bars"/>
                 </div>
             </div>
         </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import Nav from './../components/Nav';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
