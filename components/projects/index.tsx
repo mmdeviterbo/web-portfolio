c51fa7822b10712739cfd0a0629cc04a5e1b8eb5
Marty de Viterbo <mmdeviterbo@up.edu.ph>
2022-06-05 22:43:24 +0800

    refactor: correct validity types

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -4,25 +4,25 @@ import AnalyticaProject from './analytica'
 import UpQuizProject from './upquiz'
 import DashlabsProject from './dashlabs'
 import gsap from 'gsap';
-import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-gsap.registerPlugin( ScrollTrigger, ScrollToPlugin );
+// import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
+// import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+// gsap.registerPlugin( ScrollTrigger, ScrollToPlugin );
 
 const Projects=(): ReactElement => {
     const projectTextRef = useRef(null);
 
     useEffect(()=>{
-        gsap.from(projectTextRef.current,{
-            scrollTrigger: {
-                trigger: projectTextRef.current,
-                toggleActions: "restart pause reverse pause",
-                start: "top center",
-                end: "center center",
-                scrub: 1,
-            },
-            y:-100,
-            opacity: 0,
-        })
+        // gsap.from(projectTextRef.current,{
+        //     scrollTrigger: {
+        //         trigger: projectTextRef.current,
+        //         toggleActions: "restart pause reverse pause",
+        //         start: "top center",
+        //         end: "center center",
+        //         scrub: 1,
+        //     },
+        //     y:-100,
+        //     opacity: 0,
+        // })
     },[])
     
     return (
