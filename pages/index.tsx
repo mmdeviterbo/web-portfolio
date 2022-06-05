af76b16c0b7d91e0eace5342922e217f73851acb
Marty de Viterbo <mmdeviterbo@up.edu.ph>
2022-06-05 22:45:16 +0800

    refactor: correct validity types

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react';
 import HomeTitle from '../components/Home';
 import ContactSection from '../components/Home/contactSection';
 import Information from '../components/Information';
-import Projects from '../components/Projects';
+import Projects from './../components/Projects';
 import BackgroundDesign from '../components/BackgroundDesign';
 
 export default function Home() : ReactElement {
